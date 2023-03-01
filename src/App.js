ac1bb6eb8ab37b20a13cb00f7b9105bb171cd769
pawar-mahesh <pawar.mahesh2345@gmail.com>
2023-03-01 23:17:03 +0530

    src: ui: Add CardsContainer component in app.js
    Signed-off-by: pawar-mahesh <pawar.mahesh2345@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
+import CardsContainer from "./components/ui/cardsContainer/cardsContainer";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <CardsContainer />
       <Footer />
     </div>
   );
