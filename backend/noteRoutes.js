18559c443ed18c9780176417e98a4a9ff53f663a
pawar-mahesh <pawar.mahesh2345@gmail.com>
2023-03-04 23:32:19 +0530

    backend: Establish connection to mongoDB using mongoose
    Signed-off-by: pawar-mahesh <pawar.mahesh2345@gmail.com>

--- backend/noteRoutes.js
+++ backend/noteRoutes.js
@@ -1,8 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const router = express.Router();
 
+mongoose.connect("mongodb://127.0.0.1:27017/keepNote");
+
+const schema = {
+  header: String,
+  message: String,
+};
+
+const model = mongoose.model("keepNote", schema, "notes");
+
 router.get("/", (req, res) => {
-  res.send("Show all the notes");
+  model.find((err, data) => {
+    if (err) {
+      console.log("Error while fetching notes: ", err);
+    } else {
+      res.send(data);
+    }
+  });
 });
 
 router.post("/addNote", (req, res) => {
