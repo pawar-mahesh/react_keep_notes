01fb20839455d3cdc5253c6ac684618da76b92d2
pawar-mahesh <pawar.mahesh2345@gmail.com>
2023-03-07 09:52:39 +0530

    backend: Fix CORS policy: No 'Access-Control-Allow-Origin' header
    * Using cors package will resolve this issue automatically
    
    Signed-off-by: pawar-mahesh <pawar.mahesh2345@gmail.com>

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3001;
 
 const noteRoutes = require("./noteRoutes");
+
+app.use(cors());
 
 app.use("/", noteRoutes);
 
