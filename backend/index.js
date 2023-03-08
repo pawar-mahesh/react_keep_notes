791cc419228d9acb501f233256fe1e2c44231e75
pawar-mahesh <pawar.mahesh2345@gmail.com>
2023-03-08 23:54:48 +0530

    backend: index: Add bodyParser middleware
    Signed-off-by: pawar-mahesh <pawar.mahesh2345@gmail.com>

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3001;
+
+// Body-parser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const noteRoutes = require("./noteRoutes");
 
