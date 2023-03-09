b915b7c921d36cea5da7e164461c0ffb3dda3837
pawar-mahesh <pawar.mahesh2345@gmail.com>
2023-03-09 09:20:39 +0530

    frontend: card: Add extra buttons for hovering on card
    Signed-off-by: pawar-mahesh <pawar.mahesh2345@gmail.com>

--- frontend/src/components/ui/cards/card.js
+++ frontend/src/components/ui/cards/card.js
@@ -1,13 +1,39 @@
 import Card from "react-bootstrap/Card";
+import { useState } from "react";
 import "./card.css";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 
 const Cards = (props) => {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(true);
+  };
+
+  const onLeave = () => {
+    setHover(false);
+  };
+
   return (
-    <Card border="warning" style={{ width: "18rem" }} className="cards">
+    <Card
+      border="warning"
+      style={{ width: "18rem" }}
+      className="cards"
+      onMouseEnter={onHover}
+      onMouseLeave={onLeave}
+    >
       <Card.Header>{props.header}</Card.Header>
       <Card.Body>
         <Card.Text>{props.message}</Card.Text>
       </Card.Body>
+
+      {hover ? (
+        <Card.Footer>
+          <EditIcon />
+          <DeleteForeverIcon />
+        </Card.Footer>
+      ) : null}
     </Card>
   );
 };
